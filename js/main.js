075fcc1fed1941658c80d245f3a240d89b00738a
Viktoriia Hilenko <80678378+vixxxyg@users.noreply.github.com>
2024-11-15 20:53:46 +0100

    finishes

--- js/main.js
+++ js/main.js
@@ -1,20 +1,39 @@
 const btnDarkMode = document.querySelector(".dark-mode-btn");
 
-function correctThemeMode() {
+function setDarkTheme() {
     btnDarkMode.classList.add("dark-mode-btn--active");
     document.body.classList.add("dark");
 }
 
-// Comprobación del tema en localStorage y configuración del mismo modo para las demás páginas.
-if (localStorage.getItem('darkMode') === 'dark') {
-    correctThemeMode();
+function removeDarkTheme() {
+    btnDarkMode.classList.remove("dark-mode-btn--active");
+    document.body.classList.remove("dark");
 }
 
 // Comprobación del tema según la configuración del sistema del usuario.
 if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    correctThemeMode();
+    setDarkTheme();
 }
 
+// Comprobación del tema en localStorage y configuración del mismo modo para las demás páginas.
+if (localStorage.getItem("darkMode") === "dark") {
+    setDarkTheme();
+} else if (localStorage.getItem("darkMode") === "light") {
+    removeDarkTheme();
+}
+
+// Ajuste del tema en caso de cambios en la configuración del sistema.
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener('change', (event) => {
+    const newColorScheme = event.matches ? "dark" : "light";
+
+    if (newColorScheme === "dark") {
+        setDarkTheme();
+        localStorage.setItem("darkMode", "dark");
+    } else {
+        removeDarkTheme();
+        localStorage.setItem("darkMode", "light");
+    }
+})
 
 // Activación del modo nocturno mediante un botón.
 btnDarkMode.onclick = function () {
@@ -23,8 +42,8 @@ btnDarkMode.onclick = function () {
     const isDark = document.body.classList.toggle("dark");
 
     if (isDark) {
-        localStorage.setItem('darkMode', 'dark')
+        localStorage.setItem("darkMode", "dark");
     } else {
-        localStorage.setItem('darkMode', 'light')
+        localStorage.setItem("darkMode", "light");
     }
 }
