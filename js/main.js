ef4c0a285aa57052589fdbf383088377271e71cb
Viktoriia Hilenko <80678378+vixxxyg@users.noreply.github.com>
2024-11-15 19:27:52 +0100

    adds small corrections

--- js/main.js
+++ js/main.js
@@ -1,23 +1,22 @@
 const btnDarkMode = document.querySelector(".dark-mode-btn");
 
-// Comprobación del tema en localStorage y configuración del mismo modo para las demás páginas.
-
-
-
-if (localStorage.getItem('darkMode') === 'dark') {
+function correctThemeMode() {
     btnDarkMode.classList.add("dark-mode-btn--active");
     document.body.classList.add("dark");
+}
+
+// Comprobación del tema en localStorage y configuración del mismo modo para las demás páginas.
+if (localStorage.getItem('darkMode') === 'dark') {
+    correctThemeMode();
 }
 
 // Comprobación del tema según la configuración del sistema del usuario.
 if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    btnDarkMode.classList.add("dark-mode-btn--active");
-    document.body.classList.add("dark");
+    correctThemeMode();
 }
 
 
 // Activación del modo nocturno mediante un botón.
-
 btnDarkMode.onclick = function () {
     console.log('Click');
     btnDarkMode.classList.toggle("dark-mode-btn--active");
