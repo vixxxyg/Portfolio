70163769206712f5e89f7822ce1877bb83e1e7c4
Viktoriia Hilenko <80678378+vixxxyg@users.noreply.github.com>
2024-11-15 19:09:24 +0100

    adds mode check in localStorage and browser settings

--- js/main.js
+++ js/main.js
@@ -1,7 +1,31 @@
 const btnDarkMode = document.querySelector(".dark-mode-btn");
 
+// Comprobación del tema en localStorage y configuración del mismo modo para las demás páginas.
+
+
+
+if (localStorage.getItem('darkMode') === 'dark') {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+    document.body.classList.add("dark");
+}
+
+// Comprobación del tema según la configuración del sistema del usuario.
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+    document.body.classList.add("dark");
+}
+
+
+// Activación del modo nocturno mediante un botón.
+
 btnDarkMode.onclick = function () {
     console.log('Click');
     btnDarkMode.classList.toggle("dark-mode-btn--active");
-    document.body.classList.toggle("dark");
+    const isDark = document.body.classList.toggle("dark");
+
+    if (isDark) {
+        localStorage.setItem('darkMode', 'dark')
+    } else {
+        localStorage.setItem('darkMode', 'light')
+    }
 }
